a3fdbab75062281b007efa82bbad0f8874b9ccfe
BrenDev0 <lahey1991@gmail.com>
2025-08-19 17:08:18 -0600

    agent linked to user id instead of employee id , module name chage

--- src/modules/agents/agents_dependencies.py
+++ src/modules/agents/agents_dependencies.py
@@ -1,26 +1,26 @@
 from src.core.dependencies.container import Container
 from src.modules.agents.agents_service import AgentsService
 from src.modules.agents.agents_controller import AgentsController
-from src.modules.agents.employee_agents.employee_agents_repository import EmployeeAgentRepository
+from src.modules.agents.agent_access.agent_access_repository import AgentAcessRepository
 from src.core.repository.base_repository import BaseRepository
-from src.modules.agents.employee_agents.employee_agents_service import EmployeeAgentService
+from src.modules.agents.agent_access.agent_access_service import AgentAccessService
 from src.core.services.http_service import HttpService
 from src.modules.agents.agents_models import Agent
 
 def configure_agents_dependencies(http_service: HttpService):
     base_repo = BaseRepository(Agent)
-    e_a_repo = EmployeeAgentRepository()
+    a_a_repo = AgentAcessRepository()
     agents_service = AgentsService(
         respository=base_repo
     )
-    e_a_service = EmployeeAgentService(
-        repository=e_a_repo
+    a_a_service = AgentAccessService(
+        repository=a_a_repo
     )
 
     controller = AgentsController(
         http_service=http_service,
         agents_service=agents_service,
-        employee_agents_service=e_a_service
+        agent_access_service=a_a_service
     )
 
     Container.register("agents_service", agents_service)
---
+++ src/modules/chats/chats_models.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, Field, ConfigDict
+from sqlalchemy import Column, String, Text, ForeignKey
+from src.core.database.database_models import Base
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from typing import Optional, List, Dict
+from pydantic.alias_generators import to_camel
+
+class Chat(Base):
+    __tablename__ = "chats"
+    chat_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    agent_id = Column(UUID(as_uuid=True), ForeignKey("agents.agent_id", ondelete="CASCADE"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.user_id", ondelete="CASCADE"), nullable=False)
+    title = Column(Text, nullable=True)
+
+class ChatConfig(BaseModel):
+    model_config = ConfigDict(
+        populate_by_name=True,
+        serialize_by_alias=True,
+        alias_generator=to_camel 
+    )
+
+
+class ChatCreate(ChatConfig):
+    title: str
+
+class ChatUpdate(ChatCreate):
+    pass
+
+class ChatPublic(ChatCreate):
+    chat_id: uuid.UUID
+    user_id: uuid.UUID
+    agent_id: uuid.UUID
+   
+class ChatCreateResponse(ChatConfig):
+    chat_id: uuid.UUID
+
+
