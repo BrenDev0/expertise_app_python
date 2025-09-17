0d7f1571c173373dd79bf2e2675664c7175b515f
BrenDev0 <lahey1991@gmail.com>
2025-09-17 10:45:52 -0600

    encoding dynamically

--- src/modules/documents/tenant_data_service.py
+++ src/modules/documents/tenant_data_service.py
@@ -6,11 +6,16 @@ from src.core.repository.base_repository import BaseRepository
 from src.modules.documents.documents_models import TenantTable
 from src.core.decorators.service_error_handler import service_error_handler
 import os 
+import chardet
 
 class TenantDataService:
+    """
+    Handles the conversion of spreadsheet docs to sql  tables.
+    """
     __MODULE = "tenant_data.service"
     def __init__(self, repository: BaseRepository):
         self.__repository = repository
+
 
     @service_error_handler(module=__MODULE)
     def create_table_from_file(
@@ -21,14 +26,17 @@ class TenantDataService:
         filename: str,
         file_bytes: bytes
     ):
+        
+        encoding = chardet.detect(file_bytes)
+
         if filename.endswith(".csv"):
-            df = pd.read_csv(io.BytesIO(file_bytes), encoding="latin1")
+            df = pd.read_csv(io.BytesIO(file_bytes), encoding=encoding)
         elif filename.endswith((".xlsx", ".xls", ".xlsm", ".xlsb")):
-            df = pd.read_excel(io.BytesIO(file_bytes))
+            df = pd.read_excel(io.BytesIO(file_bytes), encoding=encoding)
         else:
             raise ValueError("Unsupported file type")
         
-        table_name = f"{company_id}_{os.path.splitext(filename)[0]}"
+        table_name = f"{document_id}_{os.path.splitext(filename)[0]}"
         
         ## Create a table in th db from the df
         df.to_sql(
