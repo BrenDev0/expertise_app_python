a6f74d38103fc6816717f4c30484641830355c16
BrenDev0 <lahey1991@gmail.com>
2025-10-09 15:54:52 -0600

    chat_context

--- src/modules/chats/interface/chats_dependencies.py
+++ src/modules/chats/interface/chats_dependencies.py
@@ -1,10 +1,21 @@
 from fastapi import Depends
 
-from src.modules.chats.application.chats_service import ChatsService
-from src.modules.chats.interface.chats_controller import ChatsController
 from src.core.domain.repositories.data_repository import DataRepository
 from src.modules.chats.infrastructure.sqlalchemy_chats_repository import SqlAchemyChatsRepsitory
+from src.core.domain.repositories.vector_respository import VectorRepository
+from src.core.domain.services.embedding_service import EmbeddingService
+from src.core.infrastructure.repositories.vector.redis_vector_repository import RedisVectorRepository, get_redis_client
 
+from src.modules.chats.application.chats_service import ChatsService
+from src.modules.chats.application.chat_context_service import ChatContextService
+from src.modules.chats.interface.chats_controller import ChatsController
+from src.modules.documents.interface.documents_dependencies import get_embedding_service
+
+
+def get_context_vector_repository(
+    client = Depends(get_redis_client)
+) -> VectorRepository:
+    return RedisVectorRepository(client=client)
 
 def get_chats_repository() -> DataRepository:
     return SqlAchemyChatsRepsitory()
@@ -16,9 +27,20 @@ def get_chats_service(
         repository=repository
     )
 
+def get_chat_context_service(
+    verctor_repository: VectorRepository = Depends(get_context_vector_repository),
+    embedding_service: EmbeddingService = Depends(get_embedding_service)
+) -> ChatContextService:
+    return ChatContextService(
+        embedding_service=embedding_service,
+        vector_repository=verctor_repository
+    )
+
 def get_chats_controller(
-    service: ChatsService = Depends(get_chats_service)
+    service: ChatsService = Depends(get_chats_service),
+    chat_context_service: ChatContextService = Depends(get_chat_context_service)
 ) -> ChatsController:
     return ChatsController(
-        chats_service=service
+        chats_service=service,
+        chat_context_service=chat_context_service
     )
