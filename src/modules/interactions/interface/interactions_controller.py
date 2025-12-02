02f61d6b2fcce859543c971cc0080b14a483bbb2
Brendan Lahey <lahey1991@gmail.com>
2025-12-02 11:20:18 -0600

    debug voice

--- src/modules/interactions/interface/interactions_controller.py
+++ src/modules/interactions/interface/interactions_controller.py
@@ -1,6 +1,6 @@
 from fastapi import Request
 from uuid import UUID
-
+import logging
 from src.modules.interactions.domain.interactions_models import HumanToAgentRequest
 from src.modules.chats.domain.messages_models import MessagePublic
 from src.modules.companies.domain.enitities import Company
@@ -8,6 +8,7 @@ from src.modules.companies.domain.enitities import Company
 from src.modules.users.domain.entities import User
 
 from src.modules.interactions.application.use_cases.handle_interaction import HandleInteraction
+logger = logging.getLogger(__name__)
 
 class InteractionsController:
     def __init__(
@@ -25,6 +26,7 @@ class InteractionsController:
         user: User = req.state.user
         company: Company = req.state.company
 
+        logger.info(f"INCOMMING request::::::: {data.model_dump()}")
         message = await self.__handle_interaction.execute(
             agent_id=data.agent_id,
             chat_id=chat_id,
